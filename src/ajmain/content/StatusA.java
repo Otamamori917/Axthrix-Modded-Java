2b0879e49251a2bacd5d2b51b5430c85c2431695
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-01 17:43:51 -0400

    ?

--- src/ajmain/content/StatusA.java
+++ src/ajmain/content/StatusA.java
@@ -17,19 +17,19 @@ public class StatusEffects {
     public static void load(){
 
 
-        turteI = new StatusEffect("TurteI"){{
+        StatusEffect turteI = new StatusEffect("TurteI"){{
             color = Pal.heal;
             healthMultiplier = 1.25f;
             speedMultiplier = 0.80f;
         }};
 
-        turteII = new StatusEffect("TurteII"){{
+        StatusEffect turteII = new StatusEffect("TurteII"){{
             color = Pal.heal;
             healthMultiplier = 2.50f;
             speedMultiplier = 0.40f;
         }}; 
 
-        turteIII = new StatusEffect("TurteIII"){{
+        StatusEffect turteIII = new StatusEffect("TurteIII"){{
             color = Pal.heal;
             healthMultiplier = 5f;
             speedMultiplier = 0.20f;
