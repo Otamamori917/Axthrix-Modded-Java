33abb8574e4763c1fa58e8d6129a455247d35ac7
amand <amandagraham232@gmail.com>
2023-05-01 17:57:32 -0400

    fix

--- src/ajmain/content/StatusA.java
+++ src/ajmain/content/StatusA.java
@@ -1,35 +1,27 @@
 package ajmain.content;
 
-import arc.*;
-import arc.graphics.*;
-import arc.math.*;
-import mindustry.game.EventType.*;
-import mindustry.game.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
-import mindustry.content.*;
 
-import static mindustry.Vars.*;
-
-public class StatusEffect {
+public class StatusA {
     public static StatusEffect turteI, turteII, turteIII;
 
     public static void load(){
 
 
-        StatusEffect turteI = new StatusEffect("TurteI"){{
+        turteI = new StatusEffect("TurteI"){{
             color = Pal.heal;
             healthMultiplier = 1.25f;
             speedMultiplier = 0.80f;
         }};
 
-        StatusEffect turteII = new StatusEffect("TurteII"){{
+        turteII = new StatusEffect("TurteII"){{
             color = Pal.heal;
             healthMultiplier = 2.50f;
             speedMultiplier = 0.40f;
         }}; 
 
-        StatusEffect turteIII = new StatusEffect("TurteIII"){{
+        turteIII = new StatusEffect("TurteIII"){{
             color = Pal.heal;
             healthMultiplier = 5f;
             speedMultiplier = 0.20f;
