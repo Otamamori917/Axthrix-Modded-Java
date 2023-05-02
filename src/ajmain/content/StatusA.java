99f371d72e5c236af0700b071c632145cd411296
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-02 00:17:38 -0400

    ?

--- src/ajmain/content/StatusA.java
+++ src/ajmain/content/StatusA.java
@@ -4,27 +4,50 @@ import mindustry.graphics.*;
 import mindustry.type.*;
 
 public class StatusA {
-    public static StatusEffect turteI, turteII, turteIII;
+    public static StatusEffect vindicationI, vindicationII, vindicationIII, precludedX, precludedA;
 
     public static void load(){
 
 
-        turteI = new StatusEffect("turteI"){{
+        vindicationI = new StatusEffect("vindicationI"){{
             color = Pal.heal;
             healthMultiplier = 1.25f;
             speedMultiplier = 0.80f;
         }};
 
-        turteII = new StatusEffect("turteII"){{
+        vindicationII = new StatusEffect("vindicationII"){{
             color = Pal.heal;
             healthMultiplier = 2.50f;
             speedMultiplier = 0.40f;
         }}; 
 
-        turteIII = new StatusEffect("turteIII"){{
+        vindicationIII = new StatusEffect("vindicationIII"){{
             color = Pal.heal;
             healthMultiplier = 5f;
             speedMultiplier = 0.20f;
-        }};     
+        }};
+
+
+        
+        precludedX = new StatusEffect("precludedX"){{
+            color = Color.orange;
+            speedMultiplier = 0.01f;
+            buildSpeedMultiplier = 0f;
+            reloadMultiplier = 4f;
+            damageMultiplier = 4f;
+            init(() -> {
+                opposite(precludedA);
+            });
+        }};
+
+        precludedA = new StatusEffect("precludedA"){{
+            color = Color.orange;
+            speedMultiplier = 4f;
+            buildSpeedMultiplier = 4f;
+            disarm = true;
+            init(() -> {
+                opposite(precludedX);
+            });
+        }};        
     }
 }        
