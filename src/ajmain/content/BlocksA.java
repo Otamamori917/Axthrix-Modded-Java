f34cf9cee8992771e031128c56c4e143ee44d570
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-04 15:34:39 +0100

    hmm

--- src/ajmain/content/BlocksA.java
+++ src/ajmain/content/BlocksA.java
@@ -8,6 +8,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.entities.part.*;
 import mindustry.entities.pattern.*;
+import static mindustry.Vars.*;
 
 import static mindustry.type.ItemStack.*;
 
