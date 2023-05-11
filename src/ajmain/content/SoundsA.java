33f5d248bfc5d6982781d4908d55db3bc23b0316
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-11 16:47:40 +0100

    yee

---
+++ src/ajmain/content/SoundsA.java
@@ -0,0 +1,20 @@
+package ajmain.content;
+
+import arc.audio.*;
+import arc.files.*;
+import mindustry.*;
+import mindustry.gen.*;
+import mindustry.world.blocks.power.*;
+
+import static mindustry.Vars.*;
+
+public class SoundsA{
+    public static Sound
+
+    clusterLaunch = new Sound(),
+
+    public static void load() {
+        if(Vars.headless) return;
+
+        clusterLaunch = Vars.tree.loadSound("cluster-launch");
+    }
