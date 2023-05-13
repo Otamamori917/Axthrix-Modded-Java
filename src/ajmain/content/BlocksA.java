c538861b700a28c12a033fc390edef5d87f95576
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-13 01:48:28 +0100

    lets go

--- src/ajmain/content/BlocksA.java
+++ src/ajmain/content/BlocksA.java
@@ -339,6 +339,7 @@ public class BlocksA {
             );
             inaccuracy = 0f;
             coolant = consumeCoolant(0.1f);
+            recoilTime =
 
             drawer = new DrawTurret("crystalized-"){{
                 parts.add(new RegionPart("-blade"){{
@@ -382,7 +383,7 @@ public class BlocksA {
                                 moveY = 3f;
                                 moveX = 9.5f;
 
-                                moves.add(new PartMove(PartProgress.recoil, 0f, 8f, 80f));      
+                                moves.add(new PartMove(PartProgress.recoil, 1f, 9f, 90f));      
                             }}); 
 
                             children.add(new RegionPart("-wing"){{
