451cfadabd3f8c92acd947aa748af097521c35f0
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-16 16:42:55 +0100

    yea

--- src/ajmain/starter.java
+++ src/ajmain/starter.java
@@ -14,14 +14,11 @@ public class starter extends Mod{
 
     public starter(){
         Log.info("Loaded  constructor.");
-
-        //listen for game load event
+        
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("welcome");
                 dialog.cont.add("welcome to Axthrix this mod is still in development so expect bugs").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("aj-icon")).pad(20f).row();
                 dialog.cont.button("okay", dialog::hide).size(100f, 50f);
                 dialog.show();
