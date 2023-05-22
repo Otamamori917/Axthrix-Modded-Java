2fb7304a8906d8763415b08d113454d92296f5ad
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-22 19:37:18 +0100

    hm

--- src/axthrix/content/AxthrixStatus.java
+++ src/axthrix/content/AxthrixStatus.java
@@ -39,7 +39,7 @@ public class AxthrixStatus {
             speedMultiplier = 0.80f;
             reloadMultiplier = 0.80f;
             damage = 0.4f;
-            effect = Fx.heal;
+            effect = Fx.none;
             transitionDamage = 8f;
             init(() -> {
                 opposite(vindicationI);
