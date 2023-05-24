0477eb21136f655894efa3054b4014f9bc1381ec
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-24 21:10:44 +0100

    status doesn't stay active when shooting

--- src/axthrix/content/AxthrixUnits.java
+++ src/axthrix/content/AxthrixUnits.java
@@ -163,7 +163,7 @@ public class AxthrixUnits {
 
             weapons.add(new Weapon("aj-recursor"){{
                 shootStatus = AxthrixStatus.vindicationI;
-                shootStatusDuration = 140f;
+                shootStatusDuration = 200f;
                 shootSound = Sounds.shockBlast;
                 shootWarmupSpeed = 0.06f;
                 minWarmup = 0.9f;
@@ -257,7 +257,7 @@ public class AxthrixUnits {
              weapons.add(new Weapon("aj-hammer-shotgun"){{
                 shootSound = Sounds.shockBlast;
                 shootStatus = AxthrixStatus.vindicationII;
-                shootStatusDuration = 240f;
+                shootStatusDuration = 300f;
                 shootWarmupSpeed = 0.06f;
                 minWarmup = 0.9f;
                 top = false;
