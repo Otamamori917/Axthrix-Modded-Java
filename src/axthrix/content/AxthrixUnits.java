f8f2690f538c31d82a475bda3aa7994356a3b42c
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2023-05-27 06:09:18 +0100

    WTF

--- src/axthrix/content/AxthrixUnits.java
+++ src/axthrix/content/AxthrixUnits.java
@@ -3,7 +3,7 @@ package axthrix.content;
 import arc.graphics.*;
 import arc.math.*;
 import mindustry.entities.abilities.*;
-import axthrix.types.abilitytypes.*;
+import axthrix.types.abilities.*;
 import mindustry.entities.bullet.*;
 import mindustry.entities.part.*;
 import mindustry.world.*;
